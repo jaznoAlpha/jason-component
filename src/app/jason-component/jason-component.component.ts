35766a138f9ca3de60474fe3eb7b3ff9453d505d
jaznoalpa <jason@trainsarecool.com>
2017-09-17 20:41:17 +0700

    first commit

---
+++ src/app/jason-component/jason-component.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jason-component',
+  templateUrl: './jason-component.component.html',
+  styleUrls: ['./jason-component.component.css']
+})
+export class JasonComponentComponent implements OnInit {
+  numberA: number = 1;
+  numberB: number = 2;
+  numberC: number;
+  lastOp: string = '+';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  // resultAddition() {
+  //   this.numberC=(1*(this.numberA))+(1*(this.numberB))
+  // }
+
+  // resultMultiplication() {
+  //   this.numberC=(1*(this.numberA))*(1*(this.numberB))
+  // }
+
+  calculator3() {
+    this.calculator(this.lastOp);
+  }
+
+  calculator2(event: any) { 
+    this.calculator((<HTMLInputElement>event.target).value);
+   }
+  
+  calculator(event: string) {
+    this.lastOp = event;
+    switch (event) {
+      case '+':
+        this.numberC = (1 * this.numberA) + (1 * this.numberB);
+        break;
+      case '-':
+        this.numberC = (1 * this.numberA) - (1 * this.numberB);
+        break;
+      case '*':
+        this.numberC = (1 * this.numberA) * (1 * this.numberB);
+        break;
+      case '/':
+        this.numberC = (1 * this.numberA) / (1 * this.numberB);
+        break;
+      case '%':
+        this.numberC = (1 * this.numberA) % (1 * this.numberB);
+        break;
+    }
+  }
+}
